1a3123223fcde3dca2b8e10d6286e6173d6aff5b
MichelleCordeiro <mic321@gmail.com>
2023-11-06 17:53:22 -0300

    refactor: challenge 01 - nomenclature

--- desafios/01-nomenclatura-de-variaveis.ts
+++ desafios/01-nomenclatura-de-variaveis.ts
@@ -1,50 +1,50 @@
 // Nomenclatura de variáveis
 
-const list = [
+const userCategories = [
   {
-    title: 'User',
+    userCategory: 'User',
     followers: 5
   },
   {
-    title: 'Friendly',
+    userCategory: 'Friendly',
     followers: 50,
   },
   {
-    title: 'Famous',
+    userCategory: 'Famous',
     followers: 500,
   },
   {
-    title: 'Super Star',
+    userCategory: 'Super Star',
     followers: 1000,
   },
 ]
 
 export default async function getData(req, res) {
-  const github = String(req.query.username)
+  const githubUsername = String(req.query.username)
 
-  if (!github) {
+  if (!githubUsername) {
     return res.status(400).json({
       message: `Please provide an username to search on the github API`
     })
   }
 
-  const response = await fetch(`https://api.github.com/users/${github}`);
+  const response = await fetch(`https://api.github.com/users/${githubUsername}`)
 
   if (response.status === 404) {
     return res.status(400).json({
-      message: `User with username "${github}" not found`
+      message: `User with username "${githubUsername}" not found`
     })
   }
 
-  const data = await response.json()
+  const user = await response.json()
 
-  const orderList = list.sort((a, b) =>  b.followers - a.followers); 
+  const userCategoriesLimits = userCategories.sort((a, b) =>  b.followers - a.followers)
 
-  const category = orderList.find(i => data.followers > i.followers)
+  const category = userCategoriesLimits.find(limit => user.followers > limit.followers)
 
   const result = {
-    github,
-    category: category.title
+    githubUsername,
+    category: category.userCategory
   }
 
   return result
